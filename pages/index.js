3d9ca8c8328dd5e0de9cb52218c6beda20841a25
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-22 13:04:00 +0330

    showing todos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import { getSession } from "next-auth/react";
+import HomePage from "../components/template/HomePage";
+
 export default function Home() {
-  return (
-    <>
-      <h1>todo</h1>
-    </>
-  );
+  return <HomePage />;
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
 }
